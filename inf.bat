f3549bd37c17e25ae707b090541aae3e658239b3
KusokMedi <matvejs.upesleja116@gmail.com>
2025-10-24 21:51:28 +0300

    Update inf.bat

--- inf.bat
+++ inf.bat
@@ -5,6 +5,7 @@ cls
 echo =================================================
 echo           Running your Python script...            
 echo =================================================
+echo.
 python main.py
 echo.
 echo Script finished. Restarting in 5 seconds...
